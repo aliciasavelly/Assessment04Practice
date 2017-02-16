ec4bccac767cc156adc39e848d1343962268c9e9
aliciasavelly <alicia.savelly@gmail.com>
2017-02-16 10:04:25 -0800

    migrate user comment and link and start models

--- a04prep01/skeleton/app/models/user.rb
+++ a04prep01/skeleton/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  validates :password, length: {minimum: 6}
+  validates :username, :password_digest, presence: {message: "can't be blank"}
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
