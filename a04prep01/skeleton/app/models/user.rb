fda4209d40b94321fd8e72e5dc1a51f1f31385ad
aliciasavelly <alicia.savelly@gmail.com>
2017-02-17 09:24:47 -0800

    finish user specs

--- a04prep01/skeleton/app/models/user.rb
+++ a04prep01/skeleton/app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
-  validates :password, length: {minimum: 6}
-  validates :username, :password_digest, presence: {message: "can't be blank"}
+  validates :password, length: {minimum: 6, message: "is too short (minimum is 6 characters)"}
+  validates :password_digest, presence: {message: "can't be blank"}
+  validates :session_token, :username, presence: true
+  before_validation :ensure_session_token
+  has_many :links
+  has_many :comments
 
   attr_reader :password
+
+  def generate_session_token
+    self.session_token = SecureRandom::urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.generate_session_token
+  end
 
   def password=(password)
     @password = password
