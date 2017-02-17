a9608917251461ee7843c05200b2784ff935ea26
aliciasavelly <alicia.savelly@gmail.com>
2017-02-17 09:34:37 -0800

    pass comment model specs

--- a04prep01/skeleton/app/models/comment.rb
+++ a04prep01/skeleton/app/models/comment.rb
@@ -1,2 +1,14 @@
 class Comment < ActiveRecord::Base
+  validates :body, :user_id, :link_id, presence: true
+  validates :user, presence: true
+  validates :link, presence: true
+  belongs_to :link
+  belongs_to :user
+  # def user=(user)
+  #   @user = user
+  # end
+
+  # def link=(link)
+  #   @link = link
+  # end
 end
