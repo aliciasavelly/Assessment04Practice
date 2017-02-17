aa063005262b0d9431f25408696fb5ccf8161e57
aliciasavelly <alicia.savelly@gmail.com>
2017-02-17 09:31:07 -0800

    pass link model specs

--- a04prep01/skeleton/app/models/link.rb
+++ a04prep01/skeleton/app/models/link.rb
@@ -1,2 +1,12 @@
 class Link < ActiveRecord::Base
+  validates :title, :user_id, presence: true
+  validates :url, uniqueness: true, presence: true
+  validates :user, presence: {message: "can't be blank"}
+
+  belongs_to :user
+  has_many :comments
+
+  def user=(user)
+    @user = user
+  end
 end
