341257ed5dc8cf39037715724c0dd44444817149
aliciasavelly <alicia.savelly@gmail.com>
2017-02-18 14:39:22 -0800

    add more methods for auth

--- a04prep01/skeleton/app/models/user.rb
+++ a04prep01/skeleton/app/models/user.rb
@@ -8,16 +8,32 @@ class User < ActiveRecord::Base
 
   attr_reader :password
 
-  def generate_session_token
-    self.session_token = SecureRandom::urlsafe_base64(16)
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
   end
 
   def ensure_session_token
-    self.session_token ||= self.generate_session_token
+    self.session_token ||= User.generate_session_token
   end
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest) == password
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = Usr.find_by(username: username)
+    return user if user && user.is_password?(password)
+    nil
   end
 end
