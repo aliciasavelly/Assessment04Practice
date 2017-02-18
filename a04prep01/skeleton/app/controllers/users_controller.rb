b948e26b846f6e8ab6e29708be2a690f67747b4e
aliciasavelly <alicia.savelly@gmail.com>
2017-02-18 14:32:57 -0800

    add user new view

--- a04prep01/skeleton/app/controllers/users_controller.rb
+++ a04prep01/skeleton/app/controllers/users_controller.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 class UsersController < ApplicationController
   def new
     @user = User.new
-    # render :new
+    render :new
   end
 
   def create
-    # save_and_open_page
     @user = User.new(user_params)
 
     if @user.save
       sign_in(@user)
+      redirect_to links_url
     else
       flash.now[:errors] = @user.errors.full_messages
       render :new
--- a04prep01/skeleton/config/routes.rb
+++ a04prep01/skeleton/config/routes.rb
@@ -1,3 +1,5 @@
 Links::Application.routes.draw do
   resources :users, only: [:new, :create]
+  resources :comments, only: [:index, :new, :create, :destroy, :edit, :update]
+  resources :links, only: [:index, :new, :create, :destroy, :edit, :update]
 end
