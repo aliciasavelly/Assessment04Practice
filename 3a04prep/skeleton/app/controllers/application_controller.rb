dae2e6f7ed532ed484636236453072161243f2ab
aliciasavelly <alicia.savelly@gmail.com>
2017-02-19 12:34:38 -0800

    finish 3rd try assessment 4

--- 3a04prep/skeleton/app/controllers/application_controller.rb
+++ 3a04prep/skeleton/app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_user, :logged_in?
+
   def current_user
     return nil unless session[:session_token]
-    @current_user = User.where(session_token: session[:session_token])
+    @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
   def login(user)
@@ -13,17 +15,20 @@ class ApplicationController < ActionController::Base
   end
 
   def logout
-    @current_user.reset_session_token!
-    @current_user = nil
+    current_user.reset_session_token!
+    # current_user = nil
     session[:session_token] = nil
   end
 
-  def require_logged_in
+  def require_logged_in!
     redirect_to new_session_url unless logged_in?
   end
 
   def logged_in?
-    return false unless @current_user
-    @current_user.session_token == session[:session_token]
+    # !current_user.nil?
+    # return false unless current_user
+    # @current_user.session_token == session[:session_token]
+    # true
+    !!current_user
   end
 end
--- 3a04prep/skeleton/config/routes.rb
+++ 3a04prep/skeleton/config/routes.rb
@@ -1,24 +1,6 @@
 Links::Application.routes.draw do
-  get 'links/index'
-
-  get 'links/show'
-
-  get 'links/new'
-
-  get 'links/create'
-
-  get 'links/edit'
-
-  get 'links/update'
-
-  get 'links/destroy'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  resources :users, :comments, :links
+  resources :users
+  resources :comments
+  resources :links
   resource :session
 end
