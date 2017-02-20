c4781123fffeb7b59907612d38b6554b17e44e4a
aliciasavelly <alicia.savelly@gmail.com>
2017-02-19 20:22:41 -0800

    finish attempt 6

--- 6a04prep/skeleton/config/routes.rb
+++ 6a04prep/skeleton/config/routes.rb
@@ -1,2 +1,32 @@
 Links::Application.routes.draw do
+  get 'comments/create'
+
+  get 'comments/destroy'
+
+  get 'links/index'
+
+  get 'links/show'
+
+  get 'links/new'
+
+  get 'links/create'
+
+  get 'links/edit'
+
+  get 'links/update'
+
+  get 'links/destroy'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  resources :users, :links, :comments
+  resource :session
 end
